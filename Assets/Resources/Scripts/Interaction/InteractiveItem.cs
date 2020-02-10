2b10da8e04922a821656e7666513d6403bec37dd
Nonnisi <daniel@nonit.me>
2020-02-10 03:06:53 +0000

    Basic junk

---
+++ Assets/Resources/Scripts/Interaction/InteractiveItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Custom item code, expects to be on object with an interactive item script
+public interface IInteractiveItem
+{
+    
+}
+
+//Main script to be attached to objects that need to be intractable
+public class InteractiveItem : MonoBehaviour
+{
+    public bool isEnabled = true;
+    public bool isEmitting = false;
+    public bool CanBePickedUp = false;
+    
+    //Emissions
+    public ParticleSystem particleEmission;
+
+    public void Start()
+    {
+        StartEmissions();
+    }
+
+    public void Update()
+    {
+        UpdateEmissions();
+    }
+
+    private void StartEmissions()
+    {
+        var emo = Instantiate(particleEmission, transform.position, particleEmission.transform.rotation);
+        emo.transform.SetParent(this.transform);
+        particleEmission = emo;
+    }
+
+    private void UpdateEmissions()
+    {
+        if (isEmitting && !particleEmission.isEmitting)
+        {
+            particleEmission.Play();
+        }
+        else if (!isEmitting && particleEmission.isEmitting)
+        {
+            particleEmission.Stop();
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Interaction/Items/TestItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Custom item code, expects to be on object with an interactive item script
+public class TestItem : MonoBehaviour, IInteractiveItem
+{
+    private InteractiveItem ii;
+
+    public void Start()
+    {
+        ii = GetComponent<InteractiveItem>();
+    }
+}
