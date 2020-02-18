19c5e30438d8ee178c0e38be035be2b6749f02a0
Olgidium_C <pilgrimlight.olga@gmail.com>
2020-02-18 10:27:05 +0000

    S

---
+++ Assets/Resources/Scripts/CharacterScripts/FMODPlayerAudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODPlayerAudioManager : MonoBehaviour
+{
+    private MovementController mc;
+    
+    [FMODUnity.EventRef]
+    public string WalkingGeneric;
+    public float WalkingLengthSeconds = 5.6f;
+
+
+    private float walkingTimer;
+    
+    private void Start()
+    {
+        mc = GetComponentInParent<MovementController>();
+        if (mc == null)
+        {
+            Debug.Log("FMOD Player Audio Manager could not find the movement controller!");
+        }
+
+        walkingTimer = WalkingLengthSeconds;
+    }
+
+    private void Update()
+    {
+        if (!mc.isWalking)
+        {
+            return;
+        }
+        
+        if (walkingTimer >= WalkingLengthSeconds)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot( WalkingGeneric );
+            walkingTimer = 0.0f;
+        }
+
+        walkingTimer += Time.deltaTime;
+    }
+}
