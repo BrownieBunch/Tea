4ff64b0727588a34dc5d112db0782cc2aa9f81fa
Olgidium_C <pilgrimlight.olga@gmail.com>
2020-02-18 10:44:54 +0000

    Revert "Revert "Nothing important""
    This reverts commit 54573778a4f07819df394db7596730794cc8cce3.

---
+++ Assets/Resources/Scripts/Features/InteractionDetector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionDetector : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit raycastHit;
+       // Physics.Raycast(transform.position, )
+    }
+}
